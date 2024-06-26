8069cbc3f16874912747eda0b474bbe2517ce7f1
krish-parikh06 <krish.parikh@rbslearn.net>
2024-06-26 05:01:38 +0000

    edit

---
+++ item.py
@@ -0,0 +1,10 @@
+class Item():
+    def __init(self, item_name, item_description):
+        self.name = item_name
+        self.description = item_description
+
+    def get_name(self):
+        return self.name
+
+    def set_description(self, item_description):
+        self.description = item_description
